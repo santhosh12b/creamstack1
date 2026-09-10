e44f97efa9091ace01c41ff009101c980197ae99
santhosh12b <santhosh@pixoda.co>
2026-09-10 15:05:40 +0530

    Crop Google Drive top header and fit video frame on mobile

--- src/components/DemoPage.jsx
+++ src/components/DemoPage.jsx
@@ -113,7 +113,7 @@ const DemoPage = () => {
             This happens when a user's browser blocks third-party cookies, causing Google to redirect the iframe to a login page.
           */}
           <iframe 
-            className="absolute inset-0 w-full h-full"
+            className="absolute -top-[11%] sm:-top-[10%] left-0 w-full h-[122%] sm:h-[120%] border-0"
             src="https://drive.google.com/file/d/15ZGxIEhPRHVhBfJ-1EiDAjDmlbZ_gh3m/preview" 
             frameBorder="0" 
             allow="autoplay; fullscreen; picture-in-picture"
